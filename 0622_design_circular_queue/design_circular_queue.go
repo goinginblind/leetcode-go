be4ee8ff1a9317e7b4df81f48b26ed9d0e9c6720
goinginblind <science.hhughes@gmail.com>
2025-07-02 19:58:26 +0300

    Q

--- 0622_design_circular_queue/design_circular_queue.go
+++ 0622_design_circular_queue/design_circular_queue.go
@@ -1,32 +1,74 @@
 package main
 
+import "fmt"
+
+// It came out not circular at all so need to revisit in about 5 days
 type MyCircularQueue struct {
+	queue []int
 }
 
 func Constructor(k int) MyCircularQueue {
-
+	q := make([]int, 0, k)
+	return MyCircularQueue{queue: q}
 }
 
-func (this *MyCircularQueue) EnQueue(value int) bool {
-
+func (cq *MyCircularQueue) EnQueue(value int) bool {
+	if len(cq.queue) == cap(cq.queue) {
+		return false
+	}
+	cq.queue = append(cq.queue, value)
+	return true
 }
 
-func (this *MyCircularQueue) DeQueue() bool {
-
+func (cq *MyCircularQueue) DeQueue() bool {
+	if len(cq.queue) == 0 {
+		return false
+	}
+	copy(cq.queue, cq.queue[1:])
+	cq.queue = cq.queue[:len(cq.queue)-1]
+	return true
 }
 
-func (this *MyCircularQueue) Front() int {
-
+func (cq *MyCircularQueue) Front() int {
+	if len(cq.queue) == 0 {
+		return -1
+	}
+	return cq.queue[0]
 }
 
-func (this *MyCircularQueue) Rear() int {
-
+func (cq *MyCircularQueue) Rear() int {
+	if len(cq.queue) == 0 {
+		return -1
+	}
+	return cq.queue[len(cq.queue)-1]
 }
 
-func (this *MyCircularQueue) IsEmpty() bool {
-
+func (cq *MyCircularQueue) IsEmpty() bool {
+	return len(cq.queue) == 0
 }
 
-func (this *MyCircularQueue) IsFull() bool {
+func (cq *MyCircularQueue) IsFull() bool {
+	return len(cq.queue) == cap(cq.queue)
+}
 
+func main() {
+	mcq := Constructor(3)
+	mcq.EnQueue(1) // return True
+	mcq.EnQueue(2) // return True
+	mcq.EnQueue(3) // return True
+	mcq.EnQueue(4) // return False
+	mcq.Rear()     // return 3
+	fmt.Println(mcq.queue)
+	mcq.IsFull() // return True
+	fmt.Println(mcq.queue)
+	mcq.DeQueue() // return True
+	mcq.DeQueue() // return True
+	mcq.DeQueue() // return True
+	fmt.Println(mcq.queue)
+	mcq.EnQueue(4)              // return True
+	mcq.EnQueue(5)              // return True
+	mcq.EnQueue(6)              // return True
+	fmt.Println(mcq.EnQueue(6)) // return False
+	fmt.Println(mcq.queue)
+	mcq.Rear() // return 4
 }
